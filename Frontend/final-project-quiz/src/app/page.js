1eb879885be0e2cc19be83a595d27df857982016
Eleanor Wright <elliewright055@gmail.com>
2024-05-02 14:08:23 -0500

    Additional styling

--- Frontend/final-project-quiz/src/app/page.js
+++ Frontend/final-project-quiz/src/app/page.js
@@ -3,8 +3,8 @@
 import { useState, useEffect } from "react";
 import questions from "./questions";
 
-const darkPink = "#F9476C";
-const lightPink = "#FF8195";
+const darkPink = "#f43f5e";
+const lightPink = "#fb7185";
 const tan = "#FDD5CB";
 const eggshell = "#FAF2E8";
 
@@ -57,8 +57,8 @@ function Quiz() {
   }
 
   return (
-    <div className="quiz flex-col">
-      <div className="border-2 p-2">
+    <div className="quiz flex-col rounded-md">
+      <div className="p-2">
         {shuffledQuestions.map((question, index) => (
           <Question
             key={index}
@@ -67,8 +67,8 @@ function Quiz() {
           />
         ))}
       </div>
-      <div className="flex items-center justify-center p-2 border-2 rounded-md">
-        <button className="border-2 p-2 rounded-md" onClick={handleSubmit}>
+      <div className="flex items-center justify-center p-2 rounded-md">
+        <button className="text-gray-800 font-sans border-2 border-rose-300 p-4 rounded-md bg-rose-100 hover:bg-rose-200 active:bg-rose-300 focus:outline-none focus:ring focus:ring-rose-500" onClick={handleSubmit}>
           Submit
         </button>
       </div>
@@ -91,7 +91,7 @@ function Question({ question, onAnswerSelected }) {
 
   return (
     <div>
-      <h2 className="flex question border-2 p-2 items-center justify-center rounded-md">{question.value}</h2>
+      <h2 className="flex text-rose-300 question p-2 items-center justify-center rounded-md font-sans">{question.value}</h2>
         <div className="flex space-x-4 items-center justify-center">
           {question.possibleAnswers.map((answer, index) => (
             <Button
@@ -109,7 +109,7 @@ function Question({ question, onAnswerSelected }) {
 
 function Button({ value, onClick, isSelected, color }) {
   return (
-    <button className={`button ${isSelected ? 'selected' : ''} border-2 p-6 rounded-md`} onClick={onClick} style={{backgroundColor: color}}>
+    <button className={`button ${isSelected ? 'selected' : ''} border-2 p-2 rounded-md font-sans border-rose-300 text-rose-200`} onClick={onClick} style={{backgroundColor: color}}>
       {value}
     </button>
   );
